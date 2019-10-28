953ed38eeca2668f22a3151b08b0bb65292b6cb1
Rajesh Kanduri <rajesh.kumar.reddy.kanduri@consultant.volvo.com>
2019-10-28 15:21:08 -0400

    f2

--- lwc/helloWorld/helloWorld.js
+++ lwc/helloWorld/helloWorld.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
+    //f2
     @api propBinding = 'Property Binding!'
     @track greetres='none';
     @track greettext = 'How are you?';
